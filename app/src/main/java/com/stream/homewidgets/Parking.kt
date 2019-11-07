c332015bce34ab8bddb62e3f0491d1af6db4ff83
Spencer Stream <sprewstream@gmail.com>
2019-11-06 18:59:48 -0600

    Adding code to the parking widget's onUpdate() method to hopefully grab the current floor. This should prevent the widget from losing context and displaying nothing. Also set the widget to display an empty string rather than 'null' when an error does occur.

--- app/src/main/java/com/stream/homewidgets/Parking.kt
+++ app/src/main/java/com/stream/homewidgets/Parking.kt
@@ -11,6 +11,8 @@ import android.content.Intent
  */
 class Parking : AppWidgetProvider() {
 
+    lateinit var preferences: WidgetPreferences
+
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
 
@@ -34,7 +36,12 @@ class Parking : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // Enter relevant functionality for when the widget is updated
+        preferences = WidgetPreferences(context)
+        val currentFloor = preferences.getParkingFloor()
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId, currentFloor)
+        }
     }
 
     override fun onEnabled(context: Context) {
@@ -53,7 +60,7 @@ internal fun updateAppWidget(
     currentFloor: String?
 ) {
     val parkingIcon = context.getString(R.string.parking_icon)
-    val widgetText = "$parkingIcon $currentFloor"
+    val widgetText = "$parkingIcon ${currentFloor ?: ""}"
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.parking)
     views.setTextViewText(R.id.parking_widget_text, widgetText)
