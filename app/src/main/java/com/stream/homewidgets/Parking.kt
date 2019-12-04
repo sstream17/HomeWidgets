a2af6b5ac376b3d0885a7e7da59c4fb5045075eb
Spencer Stream <sprewstream@gmail.com>
2019-12-04 09:46:52 -0600

    Adding update logic to Parking widget's onEnabled() method so the current floor will appear after a restart. I also had to remove the logic for receiving an intent

--- app/src/main/java/com/stream/homewidgets/Parking.kt
+++ app/src/main/java/com/stream/homewidgets/Parking.kt
@@ -2,9 +2,9 @@ package com.stream.homewidgets
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.widget.RemoteViews
-import android.content.Intent
 
 /**
  * Implementation of App Widget functionality.
@@ -12,24 +12,6 @@ import android.content.Intent
 class Parking : AppWidgetProvider() {
 
     lateinit var preferences: WidgetPreferences
-
-    override fun onReceive(context: Context?, intent: Intent?) {
-        super.onReceive(context, intent)
-
-        if (intent?.action.equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)){
-            val appWidgetIds = intent?.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS)
-            val currentFloor = intent?.getStringExtra("Floor_Parked")
-            context?.let {
-                val remoteViews = RemoteViews(it.packageName, R.layout.parking)
-                val appWidgetManager = AppWidgetManager.getInstance(it)
-                appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
-                // There may be multiple widgets active, so update all of them
-                for (appWidgetId in appWidgetIds!!) {
-                    updateAppWidget(context, appWidgetManager, appWidgetId, currentFloor)
-                }
-            }
-        }
-    }
 
     override fun onUpdate(
         context: Context,
@@ -45,7 +27,14 @@ class Parking : AppWidgetProvider() {
     }
 
     override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
+        val componentName = ComponentName(context, Parking::class.java)
+        val ids = AppWidgetManager.getInstance(context).getAppWidgetIds(componentName)
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        preferences = WidgetPreferences(context)
+        val currentFloor = preferences.getParkingFloor()
+        for (appWidgetId in ids) {
+            updateAppWidget(context, appWidgetManager, appWidgetId, currentFloor)
+        }
     }
 
     override fun onDisabled(context: Context) {
